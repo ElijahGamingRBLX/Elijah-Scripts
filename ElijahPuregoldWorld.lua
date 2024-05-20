63b6667ef695fea32dec93bd289329c9aa8b21d5
ElijahGamingRBLX <elijahgamingbg@gmail.com>
2024-05-20 20:29:57 +0800

    Update ElijahPuregoldWorld.lua

--- ElijahPuregoldWorld.lua
+++ ElijahPuregoldWorld.lua
@@ -1,56 +1,33 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/rileybeerblx2/kavo-ui/main/kavo.lua"))()
 
-local Window = OrionLib:MakeWindow({Name = "Elijah Hub - Puregold World", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = Library.CreateLib("Elijah Hub - Puregold World", "BloodTheme")
 
-OrionLib:MakeNotification({
-	Name = "Elijah Hub - Puregold World",
-	Content = "Elijah Hub Has Been Loaded Welcome "..Player.Name.."!",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Player")
 
---[[
-Title = <string> - The title of the notification.
-Content = <string> - The content of the notification.
-Image = <string> - The icon of the notification.
-Time = <number> - The duration of the notfication.
-]]
-local Tab = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+MainSection:NewButton("ESP Player", "ESP", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/UESP'))()
+end)
 
-Tab:AddButton({
-	Name = "Speed (50)!",
-	Callback = function()
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50 
-  	end    
-})
+MainSection:NewButton("Infinite Jump", "Gives You Inf Jump", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ElijahGamingRBLX/Elijah-Scripts/main/InfJump.lua'))()
+end)
 
-Tab:AddButton({
-	Name = "Noclip GUI",
-	Callback = function()
-      	loadstring(game:HttpGet('https://raw.githubusercontent.com/ElijahGamingRBLX/Elijah-Scripts/main/InfJump.lua'))()	
-  	end    
-})
+MainSection:NewButton("Noclip GUI", "?", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ElijahGamingRBLX/Elijah-Scripts/main/NoclipGUI.lua'))()
+end)
 
-Tab:AddButton({
-	Name = "Inf Jump",
-	Callback = function()
-      	loadstring(game:HttpGet('https://raw.githubusercontent.com/ElijahGamingRBLX/Elijah-Scripts/main/InfJump.lua'))()
-  	end    
-})
+local Player = Window:NewTab("Other")
+local PlayerSection = Tab:NewSection("Misc")
 
-local Tab = Window:MakeTab({
-	Name = "Other",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+PlayerSection:NewButton("Infinite Yield", "Infinite Yield Script", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
 
-Tab:AddButton({
-	Name = "Infinity Yield",
-	Callback = function()
-      	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-  	end    
-})
+PlayerSection:NewSlider("Change Walkspeed", "Set Walkspeed", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section:NewButton("Reset Walkspeed", "Resets To The Default Walkspeed", function()
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+end)
